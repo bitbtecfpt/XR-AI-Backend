2ce0cc2114a8be717aa161a9f6f322f52aa9548d
do.thang <100296543+DTD2604@users.noreply.github.com>
2024-12-16 14:03:47 +0700

    update connect_gpt

---
+++ app/helper/connect_gpt.py
@@ -0,0 +1,39 @@
+import openai
+
+from app.core.config import settings
+from app.helper.ChatHistoryFile import ChatHistoryFile
+from app.schemas.request.chat_bot_request import ChatBotRequest
+
+
+class ConnectGPT:
+    history_manager = ChatHistoryFile()
+
+    @staticmethod
+    def connect(request: ChatBotRequest) -> str:
+        # Lưu tin nhắn của người dùng vào lịch sử
+        ConnectGPT.history_manager.save_message(
+            session_id=request.session_id,
+            role="user",
+            message=request.prompt
+        )
+
+        openai.api_key = settings.SECRET_KEY_GPT
+        response = openai.chat.completions.create(
+            model="gpt-4o",
+            messages=ConnectGPT.history_manager.get_history(request.session_id) + [
+                {"role": "user", "content": request.prompt}
+            ],
+            temperature=1
+        )
+
+        # Lấy phản hồi từ GPT
+        bot_message = response.choices[0].message.content
+
+        # Lưu phản hồi của bot vào lịch sử
+        ConnectGPT.history_manager.save_message(
+            session_id=request.session_id,
+            role="bot",
+            message=bot_message
+        )
+
+        return bot_message
--- app/service/chat_bot_service.py
+++ app/service/chat_bot_service.py
@@ -1,11 +1,8 @@
 import logging
 
-import openai
-
-from app.helper.ChatHistoryFile import ChatHistoryFile
+from app.helper.connect_gpt import ConnectGPT
 from app.schemas.request.chat_bot_request import ChatBotRequest
 from app.schemas.response.chat_bot_response import ChatBotResponse
-from app.core.config import settings
 
 
 class ChatBotService(object):
@@ -13,37 +10,10 @@ class ChatBotService(object):
     def __init__(self) -> None:
         pass
 
-    history_manager = ChatHistoryFile()
-
     @staticmethod
     def generate_text(request: ChatBotRequest) -> ChatBotResponse:
         try:
-            # Lưu tin nhắn của người dùng vào lịch sử
-            ChatBotService.history_manager.save_message(
-                session_id=request.session_id,
-                role="user",
-                message=request.prompt
-            )
-
-            openai.api_key = settings.SECRET_KEY_GPT
-            response = openai.chat.completions.create(
-                model="gpt-4o",
-                messages=ChatBotService.history_manager.get_history(request.session_id) + [
-                    {"role": "user", "content": request.prompt}
-                ],
-                temperature=1
-            )
-
-            # Lấy phản hồi từ GPT
-            bot_message = response.choices[0].message.content
-
-            # Lưu phản hồi của bot vào lịch sử
-            ChatBotService.history_manager.save_message(
-                session_id=request.session_id,
-                role="bot",
-                message=bot_message
-            )
-
+            bot_message = ConnectGPT.connect(request)
             # Trả về nội dung phản hồi
             return ChatBotResponse(content=bot_message)
 
