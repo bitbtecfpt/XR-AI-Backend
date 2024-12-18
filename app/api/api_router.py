58a96fe1798984411aa6f0b869d84888f27ec699
vvtruong27 <vvtruong936@gmail.com>
2024-12-18 12:06:48 +0700

    fix for pm

--- app/api/api_router.py
+++ app/api/api_router.py
@@ -1,7 +1,11 @@
+# Import `APIRouter` từ FastAPI để tạo các route (endpoint) API.
 from fastapi import APIRouter
 
+# Import module `api_chat_bot` từ thư mục `api` để sử dụng các route (endpoint) liên quan đến chatbot.z
 from app.api import api_chat_bot
 
+# Khởi tạo một router mới từ FastAPI để nhóm các route liên quan đến chatbot.
 router = APIRouter()
 
+# Include các route từ `api_chat_bot` vào router hiện tại.
 router.include_router(api_chat_bot.router, tags=["chat-bot"], prefix="/chat")
