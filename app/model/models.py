88c93107a897eb2f8c9abe041a67036102fc381d
do.thang <100296543+DTD2604@users.noreply.github.com>
2024-12-24 15:31:56 +0700

    connect database

--- app/model/models.py
+++ app/model/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, DECIMAL, JSON, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, JSON, ForeignKey, Text
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from sqlalchemy.ext.declarative import declarative_base
@@ -12,26 +12,32 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(100), nullable=False)
+    gender = Column(String(5), nullable=True)
+    birth_year = Column(Integer, nullable=True)
     email = Column(String(100), unique=True, index=True, nullable=False)
-    industry = Column(String(100))
-    experience = Column(String(50))
-    target_position = Column(String(100))
+    hash_password = Column(String(100), nullable=False)
     created_at = Column(DateTime, default=func.now())
 
     # Relationships
-    skills = relationship("UserSkill", back_populates="user")
-    interviews = relationship("InterviewSession", back_populates="user")
+    interview_scripts = relationship("InterviewScript", back_populates="user")
+    interview_sessions = relationship("InterviewSession", back_populates="user")
 
 
-class UserSkill(Base):
-    __tablename__ = "user_skills"
+class InterviewScript(Base):
+    __tablename__ = "interview_script"
 
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     skill = Column(String(100), nullable=False)
+    industry = Column(String(100), nullable=False)
+    experience = Column(String(50), nullable=False)
+    target_position = Column(String(100), nullable=False)
+    JD_file = Column(String(255), nullable=True)
+    CV_file = Column(String(255), nullable=True)
+    script = Column(Text, nullable=True)
 
     # Relationship
-    user = relationship("User", back_populates="skills")
+    user = relationship("User", back_populates="interview_scripts")
 
 
 class InterviewSession(Base):
@@ -39,15 +45,12 @@ class InterviewSession(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
-    position = Column(String(100))
     date = Column(DateTime, default=func.now())
-    conversation = Column(JSON)  # hoặc Column(JSONB) nếu dùng PostgreSQL
-    overall_score = Column(DECIMAL(3, 2))
-    overall_feedback = Column(JSON)  # hoặc Column(JSONB) nếu dùng PostgreSQL
-    status = Column(String(20))
+    conversation = Column(JSON, nullable=True)  # Dùng JSONB nếu PostgreSQL
+    feedback = Column(JSON, nullable=True)  # Dùng JSONB nếu PostgreSQL
 
     # Relationship
-    user = relationship("User", back_populates="interviews")
+    user = relationship("User", back_populates="interview_sessions")
 
 
 def create_tables():
